edd8ef5da331513769e0db479ffde5e43fa84a5d
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-09 23:20:34 +0100

    Initial commit of the Solarized pack for emacs-live.

---
+++ config/solarized-config.el
@@ -0,0 +1,5 @@
+;; Solarized theme configuration file
+
+(live-add-pack-lib "solarized-emacs")
+(require 'solarized)
+(load-theme 'solarized-dark t)
---
+++ init.el
@@ -0,0 +1,3 @@
+;; Solarized pack init file
+;;
+(live-load-config-file "solarized-config.el")
